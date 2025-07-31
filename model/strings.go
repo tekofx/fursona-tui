7d5861c15de54a2e253459c8ebdfc5a22d93dd7a
tekofx <tekofxx@gmail.com>
2025-07-31 12:35:24 +0200

    Formatted other data keys

--- model/strings.go
+++ model/strings.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/tekofx/fursona-tui/style"
@@ -55,9 +56,19 @@ func GetInfoString(m Model) string {
 	if len(m.Config.OtherData) > 0 {
 		infoString += "\n" + style.Heading2.Render("Other Info") + "\n"
 		for k, v := range m.Config.OtherData {
-			infoString += fmt.Sprintf("%s: %s\n", style.Key.Render(k), style.Default.Render(v))
+
+			infoString += fmt.Sprintf("%s: %s\n", style.Key.Render(HumanizeKey(k)), style.Default.Render(v))
 		}
 	}
 
 	return infoString
+}
+func HumanizeKey(key string) string {
+	parts := strings.Split(key, "_")
+	for i, part := range parts {
+		if len(part) > 0 {
+			parts[i] = strings.ToUpper(string(part[0])) + strings.ToLower(part[1:])
+		}
+	}
+	return strings.Join(parts, " ")
 }
