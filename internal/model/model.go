631860d9d772db8759f029d5bf80404deed46d8a
tekofx <tekofxx@gmail.com>
2025-08-03 11:23:26 +0200

    Added help

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,6 +3,8 @@ package model
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -14,6 +16,8 @@ const minWidth = 100
 const minHeight = 25
 
 type Model struct {
+	keys          keyMap
+	help          help.Model
 	width         int
 	height        int
 	Config        config.Config
@@ -21,11 +25,39 @@ type Model struct {
 	imageViewPort viewport.Model
 }
 
+type keyMap struct {
+	Help key.Binding
+	Quit key.Binding
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Help, k.Quit},
+	}
+}
+
+var keys = keyMap{
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+}
+
 func InitialModel() Model {
 	textViewport := viewport.New(150, 50)
 	imageViewport := viewport.New(50, 50)
 	cfg := config.ReadConfig()
 	return Model{
+		keys:          keys,
+		help:          help.New(),
 		Config:        *cfg,
 		textViewport:  textViewport,
 		imageViewPort: imageViewport,
@@ -46,16 +78,17 @@ func (m Model) View() string {
 			Align(lipgloss.Center)
 		return style.Render(msg)
 	}
-	return lipgloss.JoinHorizontal(lipgloss.Center, " ", m.imageViewPort.View(), "  ", m.textViewport.View(), " ")
+	horizontal := lipgloss.JoinHorizontal(lipgloss.Center, " ", m.imageViewPort.View(), "  ", m.textViewport.View(), " ")
+	return lipgloss.JoinVertical(lipgloss.Center, "\n", horizontal, m.help.View(m.keys))
 
 }
 func (m *Model) sizeInputs() {
 
 	m.textViewport.Width = (m.width / 2) - 2
-	m.textViewport.Height = m.height
+	m.textViewport.Height = m.height - 2
 
 	m.imageViewPort.Width = (m.width / 2) - 2
-	m.imageViewPort.Height = m.height
+	m.imageViewPort.Height = m.height - 2
 }
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
@@ -83,11 +116,11 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch {
+		case key.Matches(msg, m.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
-		case "ctrl+z":
-			return m, tea.Suspend
 		}
 	}
 	m.sizeInputs()
